410232a9fa50b642d67447d58f7d0c9177c933ec
Alvaro Armijos <alvaro.armijoss@gmail.com>
2023-12-04 22:55:33 -0500

    Set ElevatedButton theme.

--- lib/src/ui/theme.dart
+++ lib/src/ui/theme.dart
@@ -6,12 +6,25 @@ class TrackingThemes {
 
   static final ColorScheme _colorSchemeLight = ColorScheme.light(
     primary: TrackingColors.primary,
+    background: TrackingColors.lightGrey,
+  );
+
+  static final elevatedButtonTheme = ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      shape: const StadiumBorder(),
+      backgroundColor: _colorSchemeLight.primary,
+      minimumSize: const Size.fromHeight(42),
+      disabledBackgroundColor: _colorSchemeLight.background,
+      foregroundColor: Colors.white,
+    ),
   );
 
   static ThemeData get light {
     return ThemeData.from(
       colorScheme: _colorSchemeLight,
       useMaterial3: true,
-    ).copyWith();
+    ).copyWith(
+      elevatedButtonTheme: elevatedButtonTheme,
+    );
   }
 }
